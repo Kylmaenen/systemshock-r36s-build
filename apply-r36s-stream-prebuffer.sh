9829d07a23f5b965c37f6ec5efec6911efa741a7
Kylmaenen <80922287+Kylmaenen@users.noreply.github.com>
2026-06-28 01:28:54 +0200

    Raise FluidSynth gain for quiet tracks

--- apply-r36s-stream-prebuffer.sh
+++ apply-r36s-stream-prebuffer.sh
@@ -7,4 +7,7 @@ perl -0pi -e 's#    cutscene_audiostream = SDL_NewAudioStream\(AUDIO_U8, 1, fix_
 
 perl -0pi -e 's#    cutscene_audiostream = SDL_NewAudioStream\(AUDIO_U8, 1, fix_int\(palog->a.sampleRate\), AUDIO_S16SYS, 2, snd_output_rate\(\)\);\n\n    audiolog_audiobuffer_pos = audiolog_audiobuffer;#    snd_audio_lock();\n    cutscene_audiostream = SDL_NewAudioStream(AUDIO_U8, 1, fix_int(palog->a.sampleRate), AUDIO_S16SYS, 2, snd_output_rate());\n    audiolog_audiobuffer_pos = audiolog_audiobuffer;\n    while (audiolog_audiobuffer_size > 0) {\n        int i;\n        int vol = curr_alog_vol * 127 / 100;\n        for (i = 0; i < MOVIE_DEFAULT_BLOCKLEN; i++)\n            audiolog_audiobuffer_pos[i] = 128 + ((int)audiolog_audiobuffer_pos[i] - 128) * vol / 128;\n        SDL_AudioStreamPut(cutscene_audiostream, audiolog_audiobuffer_pos, MOVIE_DEFAULT_BLOCKLEN);\n        audiolog_audiobuffer_pos += MOVIE_DEFAULT_BLOCKLEN;\n        audiolog_audiobuffer_size--;\n    }\n    snd_audio_unlock();#' src/GameSrc/audiolog.c
 
+# Raise only FluidSynth master gain; in-game music volume remains user-controlled.
+perl -0pi -e 's#fluid_settings_setnum\(settings, "synth.gain", 0\.5\);#fluid_settings_setnum(settings, "synth.gain", 0.7);#' src/MusicSrc/MusicDevice.c
+
 grep -n "while (cutscene_audiobuffer_size > 0)\|while (audiolog_audiobuffer_size > 0)" src/GameSrc/cutsloop.c src/GameSrc/audiolog.c
